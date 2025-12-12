43261971a818b68dab3d349b62f9b53c9f1f8c9f
GitHub Action <action@github.com>
2025-12-12 20:46:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/mikeburdge/tabtrail/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/mikeburdge/tabtrail/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.mikeburdge.tabtrail.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.mikeburdge.tabtrail.MyBundle
+import com.github.mikeburdge.tabtrail.services.MyProjectService
 import javax.swing.JButton
 
 
